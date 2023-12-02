3c93a960cbee45a4477e3e70cc8b39d39b4137c7
professo <you@example.com>
2023-12-02 23:00:24 +0300

    mod: part2

--- src/part2.sql
+++ src/part2.sql
@@ -8,9 +8,10 @@ DROP PROCEDURE IF EXISTS proc_must_have_project_done(peer_dep VARCHAR, task_dep 
 DROP PROCEDURE IF EXISTS proc_dependency_lookup_for_verter(id_to_check BIGINT, state_dep check_state);
 DROP TRIGGER IF EXISTS trg_person_points ON p2p CASCADE;
 DROP TRIGGER IF EXISTS trg_pre_adding_xp ON p2p CASCADE;
-DROP PROCEDURE IF EXISTS proc_adding_p2p(checked_peer_checks VARCHAR, checking_peer_p2p VARCHAR, "title_tasks" VARCHAR, state_p2p check_state, time_to_checks time);
-DROP FUNCTION IF EXISTS fnc_trg_TransferredPoints();
 
+DROP PROCEDURE IF EXISTS proc_adding_p2p(checked_peer_checks VARCHAR, checking_peer_p2p VARCHAR, "title_tasks" VARCHAR, state_p2p check_state, time_to_checks time);
+DROP FUNCTION IF EXISTS fnc_trg_TransferredPoints;
+DROP FUNCTION IF EXISTS  fnc_trg_insert_xp;
 DROP PROCEDURE IF EXISTS proc_parrent_task_done(title_for_checks varchar, peer_for_checks varchar);
 DROP PROCEDURE IF EXISTS proc_is_it_new_check(title_for_checks varchar, peer_for_checks varchar, state_for_p2p check_state);
 DROP PROCEDURE IF EXISTS proc_peers_not_eq(peer_for_checks VARCHAR(255), peer_for_p2p VARCHAR(255));
@@ -43,7 +44,7 @@ BEGIN
     );
     parrent_task_checks_id := (
         SELECT c.id
-        FROM checks c
+        FROM checks c 
         WHERE c.peer = peer_for_checks
             AND c.task = parrent_task
         ORDER BY c.date DESC
@@ -54,7 +55,7 @@ BEGIN
         FROM xp
         WHERE xp."check" = parrent_task_checks_id
     );
-    IF xp_for_parrent_task IS NULL THEN 
+    IF xp_for_parrent_task IS NULL AND parrent_task IS NOT NULL THEN ----- not null!!!!
         RAISE 'Parrent task for % is not done', title_for_checks;
     END IF;
 END;
@@ -77,7 +78,7 @@ BEGIN
         FROM checks c
         WHERE c.peer = peer_for_checks
             AND c.task = title_for_checks
-        ORDER BY c.date DESC
+        ORDER BY c.date DESC, c.id DESC
         LIMIT 1
     );
     p2p_state_for_checks_id := (
@@ -118,6 +119,7 @@ BEGIN
     END IF;
 END;
 $$;
+
 CREATE OR REPLACE PROCEDURE proc_insert_date_into_checks(
         peer_for_checks VARCHAR(255),
         title_for_checks VARCHAR(255),
@@ -126,7 +128,7 @@ CREATE OR REPLACE PROCEDURE proc_insert_date_into_checks(
 $$
 BEGIN 
     IF state_for_p2p = 'start'::check_state THEN
-        INSERT INTO checks(peer, task, date)
+        INSERT INTO checks(peer, task, "date")
         VALUES (
                 peer_for_checks,
                 title_for_checks,
@@ -136,6 +138,7 @@ BEGIN
     END IF;
 END;
 $$;
+
 CREATE OR REPLACE PROCEDURE proc_insert_date_into_p2p(
         peer_for_p2p VARCHAR(255),
         state_for_p2p check_state,
@@ -143,15 +146,22 @@ CREATE OR REPLACE PROCEDURE proc_insert_date_into_p2p(
         checks_id BIGINT
     ) LANGUAGE plpgsql AS 
 $$
+DECLARE
+    num_row BIGINT DEFAULT (SELECT count(*) FROM p2p AS p WHERE "check" = checks_id);
 BEGIN
+    IF (state_for_p2p = 'start' AND num_row = 0 ) OR (state_for_p2p != 'start' AND num_row = 1) THEN
     INSERT INTO p2p("check", checkingpeer, "state", "time")
     SELECT checks_id,
         peer_for_p2p,
         state_for_p2p,
         time_for_p2p;
     RAISE INFO 'Adding date in p2p is done';
+    ELSE
+        RAISE EXCEPTION 'It is not possible to add a record, because there is a conflict, most likely the record has already been added.';
+    END IF;
 END;
 $$;
+
 CREATE OR REPLACE PROCEDURE prc_adding_p2p(
         peer_for_checks VARCHAR(255),
         peer_for_p2p VARCHAR(255),
@@ -171,6 +181,8 @@ DECLARE BEGIN CALL proc_parrent_task_done(title_for_checks, peer_for_checks);
         FROM checks c
         WHERE c.peer = peer_for_checks
             AND c.task = title_for_checks
+            ORDER BY c."date" DESC, c.id DESC
+            LIMIT 1
     );
     CALL proc_insert_date_into_p2p(
         peer_for_p2p,
@@ -198,7 +210,7 @@ WHERE c.id = p."check"
 --
 -- adding checks && p2p
 --
-CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'start', '11:11:11');
+CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'start', current_time::time);
 
 SELECT *
 FROM checks c,
@@ -210,8 +222,7 @@ WHERE c.id = p."check"
 --
 -- adding just p2p
 --
-CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'success', '12:11:11');
-
+CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'success', current_time::time);
 SELECT *
 FROM checks c,
      p2p p
@@ -219,19 +230,40 @@ WHERE c.id = p."check"
   AND c.peer = 'pamelawalker'
   AND p.checkingpeer = 'troybrown'
   AND c.task = 'A1_MAZE';
+
+CALL prc_adding_p2p('lloydmartin', 'frankray','C7_SmartCalc_v1.0', 'start', current_time::time);
+CALL prc_adding_p2p('lloydmartin', 'frankray','C6_s21_matrix', 'start', current_time::time);
+CALL prc_adding_p2p('lloydmartin', 'frankray','C6_s21_matrix', 'fail', current_time::time);
+
+  SELECT *
+FROM checks c,
+     p2p p
+WHERE c.id = p."check"
+  AND c.peer = 'lloydmartin'
+  AND p.checkingpeer = 'frankray'
+  ORDER BY c."date", p."time";
+
+--  DELETE FROM p2p WHERE "check" = 39;
+--  DELETE FROM checks WHERE id = 38;
+
 --
 -- Test adding project that hoven-t got parrent task is done.
 --
-CALL prc_adding_p2p('pamelawalker', 'troybrown','DO3_LinuxMonitoring_v1.0', 'start', '11:11:11');
+CALL prc_adding_p2p('pamelawalker', 'troybrown','DO3_LinuxMonitoring_v1.0', 'start', current_time::time);
 --
 -- Test thats demonstrate when date with same parametrs does not insert
 --
-CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'start', '11:11:11');
+CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'start', current_time::time);
+CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'start', current_time::time);
+CALL prc_adding_p2p('pamelawalker', 'troybrown','A1_MAZE', 'success', current_time::time);
 --
 -- Test thats demonstrate when peers with same names is blocked
 --
-CALL prc_adding_p2p('frankray', 'frankray','A1_MAZE', 'start', '11:11:11');
+CALL prc_adding_p2p('frankray', 'frankray','A1_MAZE', 'start', current_time::time);
 
+
+-- CALL prc_adding_p2p('lloydmartin', 'frankray','C6_s21_matrix', 'start', '11:55:11');
+-- CALL prc_adding_p2p('lloydmartin', 'frankray', NULL, 'start', '11:55:11');
 --------------------------------------------------------
 ------------------------  ex02  ------------------------
 --------------------------------------------------------
@@ -249,18 +281,20 @@ BEGIN
                       ORDER BY p.time DESC
                       LIMIT 1);
         IF p2p_state != success_state THEN
-            RAISE 'Check p2p with the peremetrs % and % is not success. Accrual of experience blocked', peer_from_checks, task_from_checks;
+            RAISE 'Check p2p with the peremetrs ''%'' and ''%'' is not success. Accrual of experience blocked', peer_from_checks, task_from_checks;
         END IF;
-END ;
-    $$;
+END;
+$$;
 
+--------------------------------------------------------
 CREATE OR REPLACE PROCEDURE proc_is_it_new_check_verter("state_to_verter" check_state, checks_id BIGINT)
     LANGUAGE plpgsql AS
 $$
 DECLARE
     verter_state check_state := (SELECT v.state
                                  FROM verter v
-                                 WHERE v."check" = checks_id);
+                                 WHERE v."check" = checks_id
+                                 ORDER BY v.time DESC LIMIT 1);
     start_state check_state DEFAULT 'start';
 BEGIN
     IF (verter_state = start_state AND state_to_verter = start_state) OR
@@ -271,30 +305,41 @@ BEGIN
 END;
 $$;
 
+--------------------------------------------------------
 CREATE OR REPLACE PROCEDURE proc_insert_date_verter(state_to_verter check_state, time_to_verter time,
                                                     checks_id BIGINT)
     LANGUAGE plpgsql AS
 $$
+DECLARE
+    num_row_verter BIGINT DEFAULT (SELECT count(*) FROM verter AS v WHERE v.check = checks_id)::BIGINT;
 BEGIN
-    INSERT INTO verter("check", state, time)
-    VALUES (checks_id, state_to_verter, time_to_verter);
+    IF (state_to_verter = 'start' AND num_row_verter = 0) OR (state_to_verter != 'start' AND num_row_verter = 1) THEN
+    INSERT INTO verter(id, "check", "state", "time")
+    VALUES ((SELECT max(id)+1 FROM verter)::BIGINT,checks_id, state_to_verter, time_to_verter);
+    ELSE
+         RAISE EXCEPTION 'Status ''%'' for check ''%'' it cannot be recorded. Perhaps the recording is already present.',state_to_verter,checks_id;
+    END IF;
 END;
 $$;
 
-
+--------------------------------------------------------
 CREATE OR REPLACE PROCEDURE proc_adding_verter(peer_from_checks VARCHAR(255), task_from_checks VARCHAR(255),
                                                state_to_verter check_state, time_to_verter time)
     LANGUAGE plpgsql AS
 $$
 DECLARE
     checks_id BIGINT := (SELECT c.id
-                         FROM checks c
-                         WHERE c.peer = peer_from_checks
-                           AND c.task = task_from_checks);
+                         FROM checks c join p2p As p ON p.check = c.id AND p.state != 'start'
+                         WHERE c.peer = peer_from_checks 
+                           AND c.task =  task_from_checks ORDER BY c."date" DESC, p.time DESC  LIMIT 1);
 BEGIN
+    IF checks_id IS NOT NULL THEN
     CALL proc_check_in_checks_and_success_in_p2p(peer_from_checks, task_from_checks, checks_id);
     CALL proc_is_it_new_check_verter(state_to_verter, checks_id);
     CALL proc_insert_date_verter(state_to_verter, time_to_verter, checks_id);
+    ELSE
+        RAISE EXCEPTION 'The p2p verification stage for ''%'' with project ''%'' has just started or is missing',peer_from_checks,task_from_checks;
+    END IF;
 END;
 $$;
 
@@ -313,7 +358,9 @@ WHERE c.peer = 'nancywilson'
   AND c.task = 'CPP2_s21_containers'
   AND v."check" = c.id;
 
-CALL proc_adding_verter('nancywilson', 'CPP2_s21_containers', 'start', '22:22:22');
+CALL proc_adding_verter('nancywilson', 'CPP2_s21_containers', 'start', current_time::time);
+CALL proc_adding_verter('nancywilson', 'CPP2_s21_containers', 'success', current_time::time);
+
 
 SELECT *
 FROM checks c,
@@ -322,19 +369,43 @@ WHERE c.peer = 'nancywilson'
   AND c.task = 'CPP2_s21_containers'
   AND v."check" = c.id;
 
+-- DELETE FROM verter WHERE "check" = 34;
+
 --
 -- Test demonstrate that date will not insert because of conflict state.
 --
 
-CALL proc_adding_verter('nancywilson', 'CPP2_s21_containers', 'start', '22:22:22');
+CALL proc_adding_verter('nancywilson', 'CPP2_s21_containers', 'start', current_time::time);
 
 --
 -- Test demostrates that date will not be insert if p2p is fail
 --
 
-CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'start', '10:10:10');
-CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'fail', '11:10:10');
-CALL proc_adding_verter('frankray', 'A1_MAZE', 'start', '12:12:12');
+CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'start', current_time::time);
+CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'fail', current_time::time);
+CALL proc_adding_verter('frankray', 'A1_MAZE', 'start', current_time::time);
+CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'start', current_time::time);
+CALL prc_adding_p2p('frankray', 'nancywilson', 'A1_MAZE', 'success', current_time::time);
+CALL proc_adding_verter('frankray', 'A1_MAZE', 'start', current_time::time);
+CALL proc_adding_verter('frankray', 'A1_MAZE', 'success', current_time::time);
+
+SELECT *
+FROM checks c,
+     verter v
+WHERE c.peer = 'frankray'
+  AND c.task = 'A1_MAZE'
+    AND c.id = v.check
+
+SELECT DISTINCT *
+FROM checks c INNER JOIN p2p AS p ON p.check = c.id
+WHERE c.peer = 'frankray'
+  AND c.task = 'A1_MAZE'
+  ORDER BY p.time ASC;
+
+
+-- DELETE FROM verter WHERE "check"> 33;
+-- DELETE FROM p2p WHERE id > 68;
+-- DELETE FROM "checks" WHERE id > 33;
 
 
 --------------------------------------------------------
@@ -417,6 +488,7 @@ VALUES (102,'frankray','start',current_time),
 (102,'frankray','fail',current_time + '00:25:30'::TIME);
 
 SELECT * FROM transferredpoints WHERE( checkingpeer = 'lorigarrett' AND checkedpeer = 'frankray') OR  (checkedpeer  = 'lorigarrett' AND checkingpeer = 'frankray');
+
 -- DELETE FROM transferredpoints WHERE checkedpeer = 'lorigarrett' AND checkingpeer = 'frankray';
 -- DELETE FROM p2p WHERE "check" = 102;
 -- DELETE FROM checks WHERE id = 102;
@@ -462,7 +534,7 @@ EXECUTE FUNCTION fnc_trg_insert_xp();
 INSERT INTO xp ("check", XP_amount)
 VALUES (100,301);
 SELECT * FROM xp WHERE "check" = 100;
-DELETE FROM xp WHERE "check" = 100;
+
 -----
 ----- test 02
 -----
@@ -470,6 +542,7 @@ DELETE FROM xp WHERE "check" = 101;
 SELECT * FROM xp WHERE "check" = 101;
 INSERT INTO xp ("check", XP_amount)
 VALUES (101,400);
+SELECT * FROM xp AS x JOIN checks AS ch ON ch.id = x.id WHERE x."check" = 101;
 -----
 ----- test 03
 -----
@@ -483,75 +556,3 @@ INSERT INTO xp ("check", XP_amount)
 VALUES (100,300);
 SELECT * FROM xp WHERE "check" = 100;
 
-
-
-
-
-
---- proba
-
--- CREATE OR REPLACE PROCEDURE proc_adding_p2p_check(
---     _checkedpeer VARCHAR,
---     _checkingpeer VARCHAR,
---     _task VARCHAR,
---     _state check_state DEFAULT 'start',
---     _time TIME DEFAULT current_time
--- ) LANGUAGE plpgsql AS
--- $ADDING_P2P_CHECK$
--- DECLARE
---     id_for_table_checks BIGINT DEFAULT (SELECT MAX(id)+1 FROM checks)::BIGINT;
---     id_for_table_p2p BIGINT DEFAULT (SELECT MAX(id)+1 FROM p2p)::BIGINT;
---     parent_t VARCHAR DEFAULT  (SELECT parent_task FROM tasks WHERE title =  _task);
---     status_parent_task BIGINT DEFAULT 0;
-
--- BEGIN
-
---     IF  parent_t = NULL THEN
---         status_parent_task := 100;
---     ELSE
---         status_parent_task := (SELECT x.xp_amount::NUMERIC / t.max_xp * 100.0
---         FROM checks AS ch JOIN tasks AS t ON t.title = ch.task JOIN p2p AS p ON ch.id = p."check" AND p."state" = 'success' JOIN xp AS x ON x.check = ch.id
---         WHERE ch.peer = _checkedpeer AND ch.task =  parent_t  
---         AND ch.id = (select xp.check FROM xp WHERE ch.id = xp.check) ORDER BY ch."date" DESC, p."time" DESC LIMIT 1)::BIGINT;
---     END IF;
--- IF _state = 'start' AND status_parent_task > 80 AND (SELECT count(*) %2 FROM p2p WHERE "check" = (SELECT ch.id FROM p2p AS p 
---                                         INNER JOIN checks AS ch ON ch.id = p.check
---                                         WHERE ch.peer = _checkedpeer 
---                                             AND ch.task = _task
---                                             AND p.checkingpeer = _checkingpeer
---                                         ORDER BY ch."date" DESC,
---                                             p.time DESC
---                                         LIMIT 1)::BIGINT)::BIGINT = 0  THEN
---     INSERT INTO checks (id, peer, task, "date") 
---     VALUES (id_for_table_checks, _checkedpeer, _task, CURRENT_DATE);
---     INSERT INTO p2p (id, "check", checkingpeer, "state", "time")
---     VALUES (id_for_table_p2p, id_for_table_checks, _checkingpeer, _state, _time );
--- ELSIF _state != 'start' THEN 
---     id_for_table_checks := (SELECT ch.id FROM p2p AS p 
---                                         INNER JOIN checks AS ch ON ch.id = p.check
---                                         WHERE ch.peer = _checkedpeer 
---                                             AND ch.task = _task
---                                             AND p.checkingpeer = _checkingpeer
---                                         ORDER BY ch."date" DESC,
---                                             p.time DESC
---                                         LIMIT 1)::BIGINT;
---     IF (SELECT count(*) FROM p2p WHERE "check" = id_for_table_checks )::BIGINT = 1 THEN
---         INSERT INTO p2p (id, "check", checkingpeer, "state", "time")
---         VALUES (id_for_table_p2p, id_for_table_checks, _checkingpeer, _state, _time );
---     ELSIF (SELECT count(*) FROM p2p WHERE "check" = id_for_table_checks ) > 1 THEN 
---         RAISE EXCEPTION 'The VERIFICATION of the % peer with the % project has already been recorded.', _checkedpeer,_task;
---     ELSE 
---         RAISE INFO 'ERROR ERROR ERROR';
---     END IF;
--- END IF;
--- END;
--- $ADDING_P2P_CHECK$;
-
-
---     SELECT *
---     FROM checks c
---          JOIN p2p p on c.id = p."check"
--- WHERE c.peer = 'troybrown'
---   AND c.task = 'CPP2_s21_containers';
-
--- CALL proc_adding_p2p_check('troybrown', 'laurenwood', 'CPP2_s21_containers', 'fail', current_time::time);
